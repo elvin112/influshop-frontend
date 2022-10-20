a1a709b3f4eb9b75580f86b7e1cc8ec9ecb3955e
elvin112 <dasdemirovelvin1@gmail.com>
2022-10-20 19:56:01 +0200

    Add signup with validation

--- src/pages/Signin.vue
+++ src/pages/Signin.vue
@@ -1,3 +1,250 @@
 <template>
-  <h1>Signin Page</h1>
+  <main class="main">
+    <header>
+      <h2 class="header__banner">Signin</h2>
+    </header>
+    <div class="content">
+      <ui-card class="card">
+        <h1 class="logo">Influshop</h1>
+        <h3 class="lets-kick-now">
+          Let's Kick Now !
+          <span>It's Easy and takes less than 20 seconds.</span>
+        </h3>
+
+        <ui-card class="form-card">
+          <h4 class="form-card__title">Sign in</h4>
+          <form class="form-card__content">
+            <div class="input-control">
+              <input
+                type="text"
+                class="auth-input"
+                placeholder="Username"
+                name=""
+                id=""
+              />
+              <svg class="input-icon">
+                <use xlink:href="../assets/img/sprite.svg#icon-user" />
+              </svg>
+            </div>
+
+            <div class="input-control">
+              <input
+                type="password"
+                class="auth-input"
+                placeholder="Password"
+                name=""
+                id=""
+              />
+              <svg class="input-icon">
+                <use xlink:href="../assets/img/sprite.svg#icon-key" />
+              </svg>
+            </div>
+            <button type="submit" class="btn btn--primary btn--form">
+              Get Started
+            </button>
+            <p class="have-account">Have an account? <span>Sign In</span></p>
+          </form>
+        </ui-card>
+
+        <div class="return-home">
+          <svg class="return-home__icon">
+            <use xlink:href="../assets/img/sprite.svg#icon-arrow_back_ios" />
+          </svg>
+          <span>Home</span>
+        </div>
+
+        <div class="copyright">
+          <p>© Copyright 2022.</p>
+        </div>
+      </ui-card>
+    </div>
+  </main>
 </template>
+
+<script></script>
+
+<script setup></script>
+
+<style scoped>
+.main {
+  background-image: linear-gradient(0deg, rgb(246, 246, 246, 0.9) 0% 100%),
+    url("../assets/img/auth-page-background.jpg");
+  background-size: cover;
+  height: 100vh;
+  position: relative;
+}
+
+.header__banner {
+  font-size: 19.7rem;
+  font-weight: 800;
+  text-align: center;
+  color: var(--color-grey-light-1);
+  text-transform: uppercase;
+  line-height: 0.9;
+}
+
+.content {
+  position: absolute;
+  left: 50%;
+  top: 15%;
+  transform: translateX(-50%);
+}
+
+.card {
+  position: relative;
+  width: 62vw;
+  height: 78vh;
+  border-radius: var(--card-border-radius-1);
+  background-image: linear-gradient(
+    to right,
+    var(--color-primary) 0% 50%,
+    #ffffff 50% 100%
+  );
+}
+
+.logo {
+  position: absolute;
+  top: 1.4rem;
+  left: 2rem; /* This is equal to 32px */
+  font-family: "Montserrat, sans-serif";
+  font-size: 1.5rem;
+  font-weight: 800;
+  color: var(--color-grey-light-2);
+}
+
+.lets-kick-now {
+  position: absolute;
+  top: 50%;
+  left: 2rem; /* This is equal to 32px */
+  font-family: "Montserrat, sans-serif";
+  font-size: 1.4rem;
+  color: var(--color-grey-light-2);
+  transform: translateY(-50%);
+  line-height: 1.1;
+}
+
+.lets-kick-now span {
+  display: block;
+  font-size: 0.7rem;
+  color: var(--color-primary--2);
+}
+
+.form-card {
+  width: 20vw;
+  height: 53vh;
+  position: absolute;
+  left: 50%;
+  top: 20%;
+  transform: translateX(-50%);
+  border-radius: var(--card-border-radius-1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: var(--color-grey-dark-1) !important;
+}
+
+.form-card__title {
+  color: inherit;
+  text-transform: uppercase;
+  font-family: "Montserrat, sans-serif";
+  font-weight: 300;
+  font-size: 1rem;
+  padding-top: 1.4rem;
+}
+
+.form-card__title::before {
+  content: "";
+  position: absolute;
+  top: 3.6rem;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 1px;
+  width: 1.2rem;
+  background-color: var(--color-primary);
+}
+
+.form-card__content {
+  margin-top: 3rem;
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+}
+.input-control {
+  margin-bottom: 1.7rem;
+  width: 100%;
+  position: relative;
+  border-bottom: 1px solid var(--color-grey-light-3);
+}
+
+.auth-input,
+.auth-input:focus {
+  caret-color: var(--color-primary);
+  padding: 0.8rem 0;
+  outline: none !important;
+  border: 0;
+  width: 85%;
+}
+
+.input-icon {
+  position: absolute;
+  width: 1rem;
+  height: 1rem;
+  top: 0.7rem;
+  right: 0.2rem;
+  fill: transparent;
+  stroke: var(--color-grey-dark-1);
+}
+
+.btn--form {
+  width: 100%;
+  padding: 0.7rem;
+  margin-top: 1rem;
+}
+
+.have-account {
+  font-size: 0.8rem;
+  display: inline-block;
+  margin: 0 auto;
+  margin-top: 0.2rem;
+}
+
+.have-account span {
+  color: var(--color-primary);
+}
+
+.return-home {
+  position: absolute;
+  display: flex;
+  bottom: 2%;
+  left: 2rem; /* This is equal to 32px */
+  font-family: "Montserrat, sans-serif";
+  font-size: 0.7rem;
+  font-weight: 600;
+  color: var(--color-grey-light-2);
+  transform: translateY(-50%);
+  line-height: 1;
+  text-transform: uppercase;
+  align-items: center;
+}
+
+.return-home__icon {
+  fill: var(--color-grey-light-1);
+  width: 1rem;
+  height: 1rem;
+  display: inline-block;
+}
+
+.copyright {
+  position: absolute;
+  display: flex;
+  bottom: 2%;
+  right: 2rem; /* This is equal to 32px */
+  font-family: "Montserrat, sans-serif";
+  font-size: 0.6rem;
+  font-weight: 600;
+  color: var(--color-grey-dark-1);
+  transform: translateY(-50%);
+  line-height: 1;
+  align-items: center;
+}
+</style>
--- src/pages/Singup.vue
+++ src/pages/Singup.vue
@@ -10,39 +10,220 @@
           Let's Kick Now !
           <span>It's Easy and takes less than 20 seconds.</span>
         </h3>
+
         <ui-card class="form-card">
           <h4 class="form-card__title">Sign up</h4>
-          <form class="form-card__content">
-            <input
-              type="text"
-              class="auth-input"
-              placeholder="Username"
-              name=""
-              id=""
-            />
-            <input
-              type="text"
-              class="auth-input"
-              placeholder="Email"
-              name=""
-              id=""
-            />
-            <input
-              type="text"
-              class="auth-input"
-              placeholder="Password"
-              name=""
-              id=""
-            />
-            <button type="submit">Get Started</button>
+          <form class="form-card__content" @submit.prevent="submitForm">
+            <div
+              class="input-control"
+              :class="{ 'input-control--danger': !isUsernameValid }"
+            >
+              <input
+                id="username"
+                type="text"
+                class="auth-input"
+                :class="{ 'auth-input--danger': !isUsernameValid }"
+                placeholder="Username"
+                v-model="username"
+              />
+              <svg
+                class="input-icon"
+                :class="{ 'input-icon--danger': !isUsernameValid }"
+              >
+                <use xlink:href="../assets/img/sprite.svg#icon-user" />
+              </svg>
+            </div>
+            <span v-show="!isUsernameValid" class="text-danger">{{
+              usernameErrorMessage
+            }}</span>
+
+            <div
+              class="input-control"
+              :class="{ 'input-control--danger': !isEmailValid }"
+            >
+              <input
+                id="email"
+                type="email"
+                class="auth-input"
+                :class="{ 'auth-input--danger': !isEmailValid }"
+                placeholder="Email"
+                v-model="email"
+              />
+              <svg
+                class="input-icon"
+                :class="{ 'input-icon--danger': !isEmailValid }"
+              >
+                <use xlink:href="../assets/img/sprite.svg#icon-mail" />
+              </svg>
+            </div>
+            <span v-show="!isEmailValid" class="text-danger">{{
+              emailErrorMessage
+            }}</span>
+
+            <div
+              class="input-control"
+              :class="{ 'input-control--danger': !isPasswordValid }"
+            >
+              <input
+                id="password"
+                type="password"
+                class="auth-input"
+                :class="{ 'auth-input--danger': !isPasswordValid }"
+                placeholder="Password"
+                v-model="password"
+              />
+              <svg
+                class="input-icon"
+                :class="{ 'input-icon--danger': !isPasswordValid }"
+              >
+                <use xlink:href="../assets/img/sprite.svg#icon-key" />
+              </svg>
+            </div>
+            <span
+              v-show="!isPasswordValid"
+              class="text-danger text-danger-margin-bottom"
+              >Please use between 8 and 20 characters.</span
+            >
+
+            <div class="button-control">
+              <span
+                v-show="isInternalError"
+                class="text-danger text-danger--server"
+                >*Something went wrong - Please try again later.</span
+              >
+              <button type="submit" class="btn btn--primary btn--form">
+                Get Started
+              </button>
+              <p class="have-account">
+                Have an account?
+                <router-link to="/signin"><span>Sign In</span></router-link>
+              </p>
+            </div>
           </form>
         </ui-card>
+
+        <div class="return-home">
+          <svg class="return-home__icon">
+            <use xlink:href="../assets/img/sprite.svg#icon-arrow_back_ios" />
+          </svg>
+          <span>Home</span>
+        </div>
+
+        <div class="copyright">
+          <p>© Copyright 2022.</p>
+        </div>
       </ui-card>
     </div>
   </main>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      isUsernameValid: true,
+      isEmailValid: true,
+      isPasswordValid: true,
+      isFormValid: true,
+      usernameErrorMessage: "",
+      emailErrorMessage: "",
+      isInternalError: false,
+    };
+  },
+  methods: {
+    async submitForm() {
+      this.isUsernameValid = true;
+      this.isEmailValid = true;
+      this.isPasswordValid = true;
+      this.isFormValid = true;
+      this.isInternalError = false;
+
+      this.validateUsername(this.username);
+      this.validateEmail(this.email);
+      this.validatePassword(this.password);
+
+      const response = await fetch(
+        "http://localhost:8080/api/v1/auth/user/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: this.username,
+            email: this.email,
+            password: this.password,
+          }),
+        }
+      );
+
+      const data = await response.json();
+      console.log(data.status);
+      console.log(data.message);
+      if (data.status === 409) {
+        if (data.message === "Username already exists") {
+          this.usernameErrorMessage = data.message;
+          this.isUsernameValid = false;
+        } else if (data.message === "Email already exists") {
+          this.emailErrorMessage = data.message;
+          this.isEmailValid = false;
+        }
+      } else if (data.status === 500) {
+        this.isInternalError = true;
+      } else if (data.message === "User Successfully Created") {
+        alert("User Successfully Created!");
+      }
+    },
+    validateUsername(newValue) {
+      this.isUsernameValid = true;
+      if (typeof newValue != "undefined") {
+        if (newValue.length < 1 || newValue.length > 16) {
+          this.isUsernameValid = false;
+          this.isFormValid = false;
+          this.usernameErrorMessage = "Please use between 1 and 16 characters.";
+        }
+      }
+    },
+    validateEmail(newValue) {
+      this.isEmailValid = true;
+      if (typeof newValue != "undefined") {
+        if (
+          !newValue.includes("@") ||
+          newValue.length < 4 ||
+          newValue.length > 35
+        ) {
+          this.isEmailValid = false;
+          this.isFormValid = false;
+          this.emailErrorMessage = "Please enter a valid email address.";
+        }
+      }
+    },
+    validatePassword(newValue) {
+      this.isPasswordValid = true;
+      if (typeof newValue != "undefined") {
+        if (newValue.length < 8 || newValue.length > 20) {
+          this.isPasswordValid = false;
+          this.isFormValid = false;
+        }
+      }
+    },
+  },
+  watch: {
+    username(value) {
+      this.validateUsername(value);
+    },
+    email(value) {
+      this.validateEmail(value);
+    },
+    password(value) {
+      this.validatePassword(value);
+    },
+  },
+};
+</script>
 
 <script setup></script>
 
@@ -56,10 +237,10 @@
 }
 
 .header__banner {
-  font-size: 20.7rem;
+  font-size: 18.7rem;
   font-weight: 800;
   text-align: center;
-  color: var(--color-grey-light-2);
+  color: var(--color-grey-light-1);
   text-transform: uppercase;
   line-height: 0.9;
 }
@@ -67,14 +248,14 @@
 .content {
   position: absolute;
   left: 50%;
-  top: 15%;
+  top: 14%;
   transform: translateX(-50%);
 }
 
 .card {
   position: relative;
-  width: 60vw;
-  height: 72vh;
+  width: 62vw;
+  height: 78vh;
   border-radius: var(--card-border-radius-1);
   background-image: linear-gradient(
     to right,
@@ -86,36 +267,36 @@
 .logo {
   position: absolute;
   top: 1.4rem;
-  left: 3rem; /* This is equal to 42px */
+  left: 2rem; /* This is equal to 32px */
   font-family: "Montserrat, sans-serif";
-  font-size: 191.2%;
+  font-size: 1.5rem;
   font-weight: 800;
-  color: var(--color-grey-light-3);
+  color: var(--color-grey-light-2);
 }
 
 .lets-kick-now {
   position: absolute;
   top: 50%;
-  left: 3rem; /* This is equal to 42px */
+  left: 2rem; /* This is equal to 32px */
   font-family: "Montserrat, sans-serif";
-  font-size: 2rem;
-  color: var(--color-grey-light-3);
+  font-size: 1.4rem;
+  color: var(--color-grey-light-2);
   transform: translateY(-50%);
   line-height: 1.1;
 }
 
 .lets-kick-now span {
   display: block;
-  font-size: 1rem;
+  font-size: 0.7rem;
   color: var(--color-primary--2);
 }
 
 .form-card {
   width: 20vw;
-  height: 50vh;
+  height: auto;
   position: absolute;
   left: 50%;
-  top: 20%;
+  top: 19%;
   transform: translateX(-50%);
   border-radius: var(--card-border-radius-1);
   display: flex;
@@ -127,9 +308,9 @@
 .form-card__title {
   color: inherit;
   text-transform: uppercase;
-  font-family: "Manrope, sans-serif";
-  font-weight: 200;
-  font-size: 1.1rem;
+  font-family: "Montserrat, sans-serif";
+  font-weight: 300;
+  font-size: 1rem;
   padding-top: 1.4rem;
 }
 
@@ -150,17 +331,115 @@
   flex-direction: column;
   width: 80%;
 }
+.input-control {
+  margin-bottom: 1.5rem;
+  width: 100%;
+  position: relative;
+  border-bottom: 1px solid var(--color-grey-light-3);
+}
+
+.input-control--danger {
+  border-bottom: 1px solid red;
+}
 
 .auth-input,
 .auth-input:focus {
+  caret-color: var(--color-primary);
   padding: 0.8rem 0;
   outline: none !important;
   border: 0;
-  border-bottom: 1px solid var(--color-grey-light-4);
-  margin-bottom: 1.7rem;
+  width: 85%;
 }
 
-.auth-input:active::before {
-  color: var(--color-primary-light-4);
+.auth-input--danger::placeholder {
+  color: red;
+}
+
+.input-icon {
+  position: absolute;
+  width: 1rem;
+  height: 1rem;
+  top: 0.7rem;
+  right: 0.2rem;
+  fill: transparent;
+  stroke: var(--color-grey-dark-1);
+}
+
+.input-icon--danger {
+  stroke: red;
+}
+
+.button-control {
+  margin-top: 1.5rem;
+  text-align: center;
+}
+.btn--form {
+  width: 100%;
+  padding: 0.7rem;
+}
+
+.have-account {
+  font-size: 0.8rem;
+  display: inline-block;
+  margin: 0 auto;
+  margin-top: 0.2rem;
+  margin-bottom: 1.6rem;
+}
+
+.have-account span,
+a {
+  color: var(--color-primary);
+  text-decoration: none;
+}
+
+.return-home {
+  position: absolute;
+  display: flex;
+  bottom: 2%;
+  left: 2rem; /* This is equal to 32px */
+  font-family: "Montserrat, sans-serif";
+  font-size: 0.7rem;
+  font-weight: 600;
+  color: var(--color-grey-light-2);
+  transform: translateY(-50%);
+  line-height: 1;
+  text-transform: uppercase;
+  align-items: center;
+}
+
+.return-home__icon {
+  fill: var(--color-grey-light-1);
+  width: 1rem;
+  height: 1rem;
+  display: inline-block;
+}
+
+.copyright {
+  position: absolute;
+  display: flex;
+  bottom: 2%;
+  right: 2rem; /* This is equal to 32px */
+  font-family: "Montserrat, sans-serif";
+  font-size: 0.6rem;
+  font-weight: 600;
+  color: var(--color-grey-dark-1);
+  transform: translateY(-50%);
+  line-height: 1;
+  align-items: center;
+}
+
+.text-danger {
+  font-family: "Montserrat, sans-serif";
+  font-size: 0.7rem;
+  margin-bottom: 0.8rem;
+  margin-top: -1.3rem;
+  color: red;
+}
+
+.text-danger--server {
+  margin-top: 0.5rem;
+}
+.text-danger-margin-bottom {
+  margin-bottom: 0.8rem;
 }
 </style>

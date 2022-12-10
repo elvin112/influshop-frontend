fff3d8117d75eb1c140a4f3c12ecaf8bd081f37c
elvin112 <dasdemirovelvin1@gmail.com>
2022-12-10 10:48:46 +0100

    Fix bug in Cart page

--- src/store/modules/cart/getters.js
+++ src/store/modules/cart/getters.js
@@ -2,6 +2,9 @@ export default {
   userCart(state) {
     return state.userCart;
   },
+  cartTotalPrice(state) {
+    return state.totalPrice;
+  },
   cartLength(state) {
     return state.userCart.length;
   },
